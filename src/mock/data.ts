26038cf39347d80a852b8bea124319444eeb84d0
Yeray Lage <ylagef@gmail.com>
2022-09-06 19:14:24 +0200

    feat: add section layout and allergens icons

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -1,43 +1,186 @@
-export interface Section {
-  title: string
-  id: string
+export enum EXTRA_SERVICES {
+  BREAD = 'Servicio de pan 1,50€',
+  GLUTEN_FREE_BREAD = 'Servicio de pan sin gluten 1,50€',
+  RYE_BREAD = 'Opción de pan de centeno +1€',
+  GLUTEN_FREE_BUN = 'Bollo sin gluten +1€'
 }
 
-export const SECTIONS: Section[] = [
+export enum SCHEDULES {
+  BREAKFAST = 'Horario de desayunos de 8:00 a 12:30',
+  SNACK = 'Horario de meriendas de 17:00 a 20:00',
+  UNINTERRUPTED = 'Cocina ininterrumpida de 8:00 a 23:30'
+}
+
+export enum ALLERGENS {
+  CELERY = 'celery',
+  CRUSTACEANS = 'crustaceans',
+  EGG = 'egg',
+  FISH = 'fish',
+  GLUTEN = 'gluten',
+  LACTOSE = 'lactose',
+  LUPINS = 'lupins',
+  MOLLUSCS = 'molluscs',
+  MUSTARD = 'mustard',
+  NUTS = 'nuts',
+  PEELING_FRUITS = 'peeling_fruits',
+  SESAME = 'sesame',
+  SHELLFISH = 'shellfish',
+  SOY = 'soy',
+  SULPHITES = 'sulphites'
+}
+
+export interface Product {
+  id: string
+  name: string
+  description?: string
+  options?: string[]
+  price: number | string // String for "S/M"
+  allergens?: ALLERGENS[]
+}
+
+export interface Category {
+  id: string
+  label: string
+  extraServices?: EXTRA_SERVICES[]
+  products: Product[]
+}
+
+export interface Section {
+  id: string
+  buttonText: string
+  sectionTitle?: string
+  schedules?: string[]
+  extraServices?: string[]
+  categories?: Category[]
+  products?: Product[]
+}
+
+export const GASTRO_SECTIONS: Section[] = [
   {
-    title: 'Menú del día',
-    id: 'menu'
+    id: 'menu',
+    buttonText: 'Menú',
+    sectionTitle: 'Menú del día'
   },
   {
-    title: 'Desayunos y meriendas',
-    id: 'desayunos-meriendas'
+    id: 'desayunos-meriendas',
+    buttonText: 'Desayunos y meriendas',
+    sectionTitle: 'Desayunos y meriendas',
+    schedules: [SCHEDULES.BREAKFAST, SCHEDULES.SNACK],
+    categories: [
+      {
+        id: 'clasicas',
+        label: 'Clásicas',
+        extraServices: [EXTRA_SERVICES.RYE_BREAD, EXTRA_SERVICES.GLUTEN_FREE_BUN],
+        products: [
+          {
+            id: 'aove',
+            name: 'De AOVE',
+            price: 2,
+            allergens: [ALLERGENS.GLUTEN]
+          },
+          {
+            id: 'confitura-temporada-mantequilla',
+            name: 'De confitura de temporada y mantequilla',
+            price: 2.5,
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE]
+          },
+          {
+            id: 'tomate-aove',
+            name: 'De tomate restregado y AOVE',
+            price: 3,
+            allergens: [ALLERGENS.GLUTEN]
+          }
+        ]
+      },
+      {
+        id: 'del-auriga',
+        label: 'Las tostas del Auriga',
+        products: [
+          {
+            id: 'mex',
+            name: 'Taco mex,  guacamole, tartar de ahumados y queso galmesano',
+            price: 6.5,
+            allergens: [ALLERGENS.LACTOSE, ALLERGENS.FISH]
+          },
+          {
+            id: 'centeno-requeson',
+            name: 'Tosta de centeno, requesón, fruta de hueso a la parrilla, nueces y miel',
+            price: 6.5,
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGG]
+          },
+          {
+            id: 'cea-huevos',
+            name: 'Tosta de Cea, huevos BT, espinaca fresca y champiñones encurtidos',
+            price: 6.5,
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE]
+          }
+        ]
+      },
+      {
+        id: 'los-huevos',
+        label: 'Los huevos',
+        products: [
+          {
+            id: 'tortilla-patata',
+            name: 'Tortilla de patata',
+            description: 'Pintxo hecho al momento con pan con tomate',
+            price: 3,
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.EGG]
+          },
+          {
+            id: 'croissant-huevo',
+            name: 'Croissant a la plancha, huevos revueltos y jamón crujiente',
+            price: 6.5,
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGG, ALLERGENS.SULPHITES]
+          },
+          {
+            id: 'benedictine',
+            name: '63ºC con lacón',
+            price: 7,
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGG, ALLERGENS.SULPHITES]
+          }
+        ]
+      }
+    ]
   },
   {
-    title: 'Masas y panes',
-    id: 'masas-panes'
+    id: 'masas-panes',
+    buttonText: 'Masas y panes',
+    sectionTitle: 'Con pan y mucha miga',
+    schedules: [SCHEDULES.UNINTERRUPTED]
   },
   {
-    title: 'Pintxos',
-    id: 'pintxos'
+    id: 'pintxos',
+    buttonText: 'Pintxos',
+    sectionTitle: 'Somos una ración',
+    schedules: [SCHEDULES.UNINTERRUPTED],
+    extraServices: [EXTRA_SERVICES.BREAD, EXTRA_SERVICES.GLUTEN_FREE_BREAD]
   },
   {
-    title: 'Raciones',
-    id: 'raciones'
+    id: 'raciones',
+    buttonText: 'Raciones',
+    schedules: [SCHEDULES.UNINTERRUPTED],
+    extraServices: [EXTRA_SERVICES.BREAD, EXTRA_SERVICES.GLUTEN_FREE_BREAD]
   },
   {
-    title: 'Postres',
-    id: 'postres'
+    id: 'postres',
+    buttonText: 'Postres',
+    sectionTitle: 'Los dulces',
+    schedules: [SCHEDULES.UNINTERRUPTED]
   },
   {
-    title: 'Recomendaciones',
-    id: 'recomendaciones'
+    id: 'recomendaciones',
+    buttonText: 'Recomendaciones',
+    sectionTitle: 'Lo que hoy nos inspira'
   },
   {
-    title: 'Vinos',
-    id: 'vinos'
+    id: 'vinos',
+    buttonText: 'Vinos',
+    sectionTitle: 'Vinos'
   },
   {
-    title: 'Coctelería',
-    id: 'cocteleria'
+    id: 'cocteleria',
+    buttonText: 'Coctelería',
+    sectionTitle: 'Coctelería'
   }
 ]
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'light-text': '#ffffff',
+        'dark-text': '#151515',
+        'card-bg': '#dedede'
+      },
+      fontSize: {
+        xxs: '.85rem' // 14px
+      }
+    }
   },
   plugins: []
 }
