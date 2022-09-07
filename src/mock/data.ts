eac2a45e1f3795bdf0ff63bd9b76ede5f27f4a3e
Yeray Lage <ylagef@gmail.com>
2022-09-07 09:58:19 +0200

    feat: generalize restaurant config

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -14,7 +14,7 @@ export enum SCHEDULES {
 export enum ALLERGENS {
   CELERY = 'celery',
   CRUSTACEANS = 'crustaceans',
-  EGG = 'egg',
+  EGGS = 'eggs',
   FISH = 'fish',
   GLUTEN = 'gluten',
   LACTOSE = 'lactose',
@@ -24,13 +24,19 @@ export enum ALLERGENS {
   NUTS = 'nuts',
   PEELING_FRUITS = 'peeling_fruits',
   SESAME = 'sesame',
-  SHELLFISH = 'shellfish',
   SOY = 'soy',
   SULPHITES = 'sulphites'
 }
 
+export interface Restaurant {
+  id: string
+  name: string
+  shortName?: string
+}
+
 export interface Product {
   id: string
+  order: number
   name: string
   description?: string
   options?: string[]
@@ -40,6 +46,7 @@ export interface Product {
 
 export interface Category {
   id: string
+  order: number
   label: string
   extraServices?: EXTRA_SERVICES[]
   products: Product[]
@@ -69,23 +76,27 @@ export const GASTRO_SECTIONS: Section[] = [
     categories: [
       {
         id: 'clasicas',
+        order: 0,
         label: 'Clásicas',
         extraServices: [EXTRA_SERVICES.RYE_BREAD, EXTRA_SERVICES.GLUTEN_FREE_BUN],
         products: [
           {
             id: 'aove',
+            order: 0,
             name: 'De AOVE',
             price: 2,
             allergens: [ALLERGENS.GLUTEN]
           },
           {
             id: 'confitura-temporada-mantequilla',
+            order: 1,
             name: 'De confitura de temporada y mantequilla',
             price: 2.5,
             allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE]
           },
           {
             id: 'tomate-aove',
+            order: 2,
             name: 'De tomate restregado y AOVE',
             price: 3,
             allergens: [ALLERGENS.GLUTEN]
@@ -94,22 +105,26 @@ export const GASTRO_SECTIONS: Section[] = [
       },
       {
         id: 'del-auriga',
+        order: 1,
         label: 'Las tostas del Auriga',
         products: [
           {
             id: 'mex',
+            order: 0,
             name: 'Taco mex,  guacamole, tartar de ahumados y queso galmesano',
             price: 6.5,
             allergens: [ALLERGENS.LACTOSE, ALLERGENS.FISH]
           },
           {
             id: 'centeno-requeson',
+            order: 1,
             name: 'Tosta de centeno, requesón, fruta de hueso a la parrilla, nueces y miel',
             price: 6.5,
-            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGG]
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGGS]
           },
           {
             id: 'cea-huevos',
+            order: 2,
             name: 'Tosta de Cea, huevos BT, espinaca fresca y champiñones encurtidos',
             price: 6.5,
             allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE]
@@ -118,26 +133,30 @@ export const GASTRO_SECTIONS: Section[] = [
       },
       {
         id: 'los-huevos',
+        order: 2,
         label: 'Los huevos',
         products: [
           {
             id: 'tortilla-patata',
+            order: 0,
             name: 'Tortilla de patata',
             description: 'Pintxo hecho al momento con pan con tomate',
             price: 3,
-            allergens: [ALLERGENS.GLUTEN, ALLERGENS.EGG]
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.EGGS]
           },
           {
             id: 'croissant-huevo',
+            order: 1,
             name: 'Croissant a la plancha, huevos revueltos y jamón crujiente',
             price: 6.5,
-            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGG, ALLERGENS.SULPHITES]
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGGS, ALLERGENS.SULPHITES]
           },
           {
             id: 'benedictine',
+            order: 2,
             name: '63ºC con lacón',
             price: 7,
-            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGG, ALLERGENS.SULPHITES]
+            allergens: [ALLERGENS.GLUTEN, ALLERGENS.LACTOSE, ALLERGENS.EGGS, ALLERGENS.SULPHITES]
           }
         ]
       }
@@ -147,40 +166,53 @@ export const GASTRO_SECTIONS: Section[] = [
     id: 'masas-panes',
     buttonText: 'Masas y panes',
     sectionTitle: 'Con pan y mucha miga',
-    schedules: [SCHEDULES.UNINTERRUPTED]
+    schedules: [SCHEDULES.UNINTERRUPTED],
+    products: []
   },
   {
     id: 'pintxos',
     buttonText: 'Pintxos',
     sectionTitle: 'Somos una ración',
     schedules: [SCHEDULES.UNINTERRUPTED],
-    extraServices: [EXTRA_SERVICES.BREAD, EXTRA_SERVICES.GLUTEN_FREE_BREAD]
+    extraServices: [EXTRA_SERVICES.BREAD, EXTRA_SERVICES.GLUTEN_FREE_BREAD],
+    products: []
   },
   {
     id: 'raciones',
     buttonText: 'Raciones',
     schedules: [SCHEDULES.UNINTERRUPTED],
-    extraServices: [EXTRA_SERVICES.BREAD, EXTRA_SERVICES.GLUTEN_FREE_BREAD]
+    extraServices: [EXTRA_SERVICES.BREAD, EXTRA_SERVICES.GLUTEN_FREE_BREAD],
+    products: []
   },
   {
     id: 'postres',
     buttonText: 'Postres',
     sectionTitle: 'Los dulces',
-    schedules: [SCHEDULES.UNINTERRUPTED]
+    schedules: [SCHEDULES.UNINTERRUPTED],
+    products: []
   },
   {
     id: 'recomendaciones',
     buttonText: 'Recomendaciones',
-    sectionTitle: 'Lo que hoy nos inspira'
+    sectionTitle: 'Lo que hoy nos inspira',
+    products: []
   },
   {
     id: 'vinos',
     buttonText: 'Vinos',
-    sectionTitle: 'Vinos'
+    sectionTitle: 'Vinos',
+    products: []
   },
   {
     id: 'cocteleria',
     buttonText: 'Coctelería',
-    sectionTitle: 'Coctelería'
+    sectionTitle: 'Coctelería',
+    products: []
   }
 ]
+
+export const RESTAURANT: Restaurant = {
+  id: 'auriga',
+  name: 'Auriga gastrobar',
+  shortName: 'Auriga'
+}
