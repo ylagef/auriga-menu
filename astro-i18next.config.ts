55d421c5ca442f776a4669d816c0293b917f98bc
Yeray Lage <ylagef@gmail.com>
2022-09-07 09:55:53 +0200

    chore: add i18n translations

---
+++ astro-i18next.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLanguage: 'es',
+  supportedLanguages: ['en', 'es'],
+  i18next: {
+    debug: true,
+    initImmediate: false,
+    backend: {
+      loadPath: './src/locales/{{lng}}.json'
+    }
+  },
+  i18nextPlugins: { fsBackend: 'i18next-fs-backend' }
+}
