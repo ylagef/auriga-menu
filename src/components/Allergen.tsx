b5a54de5062d8bfbc583f56e1e9a526165ada420
Yeray Lage <ylagef@gmail.com>
2022-09-07 14:51:54 +0200

    fix: allergen close on timeout

--- src/components/Allergen.tsx
+++ src/components/Allergen.tsx
@@ -1,5 +1,5 @@
 import { ALLERGENS } from '../types'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export interface Props {
   allergen: ALLERGENS
@@ -9,15 +9,23 @@ export interface Props {
 export function Allergen({ allergen, text }: Props) {
   const [allergenOpen, setAllergenOpen] = useState<ALLERGENS>(null)
 
+  useEffect(() => {
+    if (!allergenOpen) return
+
+    const timeout = setTimeout(() => {
+      setAllergenOpen(null)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [allergenOpen])
   return (
     <div
       className="flex items-center overflow-x-hidden"
       onClick={() => {
-        setAllergenOpen(allergenOpen === allergen ? null : allergen)
+        setAllergenOpen((prev) => (prev === allergen ? null : allergen))
       }}
     >
       <img className="h-6" src={`/assets/images/allergens/${allergen}.png`} alt={allergen} />
-      <small className={`${allergenOpen === allergen ? 'w-screen px-2' : 'w-0'} transition-all max-w-fit`}>{text}</small>
+      <small className={`${allergenOpen === allergen ? 'w-screen pl-2' : 'w-0'} transition-all max-w-fit`}>{text}</small>
     </div>
   )
 }
