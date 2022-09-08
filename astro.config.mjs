c4ba4a8b3e6229cb51335dfdb6debe4659fbf09e
Yeray Lage <ylagef@gmail.com>
2022-09-08 14:03:40 +0200

    fix: remove getStaticPaths from admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
+  output: 'server',
   adapter: netlify()
-});
+})
