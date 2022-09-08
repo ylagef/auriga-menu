b6d058afd87dcd6768743e8db37918ed7f2561a7
Yeray Lage <ylagef@gmail.com>
2022-09-08 09:21:51 +0200

    feat: remove translation module (not needed)

---
+++ src/locales/translations.ts
@@ -0,0 +1,18 @@
+export const translations = {
+  allergens: {
+    celery: 'Apio',
+    crustaceans: 'Crustáceos',
+    eggs: 'Huevos',
+    fish: 'Pescado',
+    gluten: 'Gluten',
+    lactose: 'Lactosa',
+    lupins: 'Altramuces',
+    molluscs: 'Moluscos',
+    mustard: 'Mostaza',
+    nuts: 'Frutos secos',
+    peeling_fruits: 'Frutos de cáscara',
+    sesame: 'Sésamo',
+    soy: 'Soja',
+    sulphites: 'Sulfitos'
+  }
+}
