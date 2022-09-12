b802ce2020513bc4a618832982fcda5a335f17be
Yeray Lage <ylagef@gmail.com>
2022-09-13 00:30:58 +0200

    feat: handle login and auth with supabase

---
+++ src/utils/cookies.ts
@@ -0,0 +1,19 @@
+export const setCookie = (name, value, expiresIn, domain) => {
+  let expires = ''
+  if (expiresIn) {
+    const date = new Date()
+    date.setTime(date.getTime() + expiresIn)
+    expires = '; expires=' + date.toUTCString()
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; path=/; domain=' + domain
+}
+export const getCookie = (name) => {
+  const nameEQ = name + '='
+  const ca = document.cookie.split(';')
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length)
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
+  }
+  return null
+}
