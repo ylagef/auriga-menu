f94da87f20b4eae0efb2c17955324ce7c60ba2aa
Yeray Lage <ylagef@gmail.com>
2022-09-15 13:31:51 +0200

    feat: move allergen name to title

--- src/components/AllergenSelector.tsx
+++ src/components/AllergenSelector.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import { translations } from 'src/locales/translations'
 
 import type { ALLERGENS } from '../types'
@@ -9,15 +8,14 @@ export interface Props {
   isSelected: boolean
 }
 
-export default function AllergenSelector({ allergen, onClick, isSelected }: Props): JSX.Element {
+export default function AllergenSelector({ allergen, onClick, isSelected }: Props) {
   return (
-    <div className="flex flex-col justify-center items-center pointer" onClick={onClick}>
+    <div className="flex flex-col justify-center items-center pointer" onClick={onClick} title={translations.allergens[allergen]}>
       <img
         className={`h-12 ${!isSelected ? 'opacity-40' : ''}`}
         src={`/assets/images/allergens/${allergen}.png`}
         alt={translations.allergens[allergen]}
       />
-      <small className="pt-2 text-xs">{translations.allergens[allergen]}</small>
     </div>
   )
 }
