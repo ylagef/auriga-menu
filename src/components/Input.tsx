113b5c5309281c284dfc66ffeaaa4ee118859e5f
Yeray Lage <ylagef@gmail.com>
2022-09-16 19:27:57 +0200

    feat: price input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function Input({
   id,
@@ -7,7 +7,8 @@ export function Input({
   type = 'text',
   required = false,
   defaultValue,
-  onChange
+  onChange,
+  customValue = false
 }: {
   id: string
   label?: string
@@ -16,7 +17,10 @@ export function Input({
   required?: boolean
   defaultValue?: string
   onChange?: (evt: React.ChangeEvent<HTMLInputElement>) => void
+  customValue?: boolean
 }) {
+  const [customEnabled, setCustomEnabled] = useState(false)
+
   if (type === 'textarea') {
     return (
       <div className="flex flex-col gap-1 w-full">
@@ -36,12 +40,29 @@ export function Input({
 
   return (
     <div className="flex flex-col gap-1 w-full">
-      <label htmlFor={id}>
-        {label} {required && '*'}
-      </label>
+      <div className="flex justify-between">
+        <label htmlFor={id}>
+          {label} {required && '*'}
+        </label>
+
+        {customValue && (
+          <div className="flex gap-2 items-center justify-center">
+            <label htmlFor={`${id}-custom-value`}>Personalizado</label>
+            <input
+              type="checkbox"
+              name={`${id}-custom-value`}
+              id={`${id}-custom-value`}
+              className="w-5 h-5"
+              onChange={(ev) => {
+                setCustomEnabled(ev.target.checked)
+              }}
+            />
+          </div>
+        )}
+      </div>
       <input
         className="border-b-dark-text py-2 px-4 rounded"
-        type={type}
+        type={customEnabled ? 'text' : type}
         name={id}
         placeholder={placeholder}
         defaultValue={defaultValue}
