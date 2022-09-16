96e427a302138ca035eb09da660fbb80eb0ff290
Yeray Lage <ylagef@gmail.com>
2022-09-16 20:33:56 +0200

    feat: allergen selector drop shadow

--- src/components/AllergenSelector.tsx
+++ src/components/AllergenSelector.tsx
@@ -12,7 +12,7 @@ export default function AllergenSelector({ allergen, onClick, isSelected }: Prop
   return (
     <div className="flex flex-col justify-center items-center pointer" onClick={onClick} title={translations.allergens[allergen]}>
       <img
-        className={`transition-opacity h-12 ${isSelected ? 'opacity-100' : 'opacity-40'}`}
+        className={`transition-all h-12 ${isSelected ? 'opacity-100 drop-shadow-allergen' : 'opacity-40'}`}
         src={`/assets/images/allergens/${allergen}.png`}
         alt={translations.allergens[allergen]}
       />
