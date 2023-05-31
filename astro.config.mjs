5224c49470df844c21fa5ac8f036b69294ebfc41
Yeray Lage <ylagef@gmail.com>
2023-05-31 11:01:04 +0200

    feat: add astro compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import netlify from '@astrojs/netlify/functions';
 
-import netlify from '@astrojs/netlify/functions'
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), compress()],
   output: 'server',
   adapter: netlify()
-})
+});
