459465fa3a39dfb6d290ccfb57ed0bd6030af785
Yeray Lage <ylagef@gmail.com>
2023-05-31 11:08:05 +0200

    fix: header width overflow

--- src/components/app/BurgerMenu.tsx
+++ src/components/app/BurgerMenu.tsx
@@ -41,12 +41,12 @@ export default function BurgerMenu({ dark = false }: Props) {
   if (!sectionSlug || (sectionSlug && sectionSlug === 'login')) return null
 
   return (
-    <div className={`w-screen absolute overflow-x-hidden top-0 left-0 ${isOpen ? 'h-full' : 'h-20'}`}>
+    <div className={`w-full absolute overflow-x-hidden top-0 left-0 ${isOpen ? 'h-full' : 'h-20'}`}>
       <button id="menu" className="absolute top-4 right-4 py-2 px-4 rounded w-fit z-20" onClick={handleOpen}>
         <MenuIcon dark={dark} opened={isOpen} />
       </button>
 
-      <div className={`absolute top-0 w-screen h-full bg-black/80 p-4 transition-[left] overflow-y-auto ${isOpen ? 'left-0' : 'left-full'}`}>
+      <div className={`absolute top-0 w-full h-full bg-black/80 p-4 transition-[left] overflow-y-auto ${isOpen ? 'left-0' : 'left-full'}`}>
         <div className="flex flex-col items-center gap-4 mt-16">
           {categories
             ?.sort((a, z) => a.order - z.order)
